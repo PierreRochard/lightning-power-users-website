e81583093367f32dedcd23959241c88247c6bf99
Pierre Rochard <pierre@rochard.org>
2019-02-17 20:22:29 -0500

    refactor invoice emitter

--- websocket/invoice_emitter.py
+++ websocket/invoice_emitter.py
@@ -1,19 +1,36 @@
 import asyncio
 import json
-import threading
 
 import websockets
 from google.protobuf.json_format import MessageToDict
 
+from lnd_grpc import lnd_grpc
 from node_launcher.logging import log
-from tools.lnd_client import lnd_remote_client
 from websocket.utilities import get_server_id
 
+DEFAULT_WEBSOCKET_URL = 'ws://localhost:8765'
 
-def emit_invoices():
-    async def send_to_server():
-        async with websockets.connect('ws://localhost:8765') as websocket:
-            invoice_subscription = lnd_remote_client.subscribe_invoices(settle_index=1)
+
+class InvoiceEmitter(object):
+    def __init__(self,
+                 websocket_url: str = DEFAULT_WEBSOCKET_URL,
+                 lnd_dir: str = None,
+                 lnd_network: str = 'mainnet',
+                 lnd_grpc_host: str = 'localhost',
+                 lnd_grpc_port: str = '10011'):
+        self.rpc = lnd_grpc.Client(
+            lnd_dir=lnd_dir,
+            network=lnd_network,
+            grpc_host=lnd_grpc_host,
+            grpc_port=lnd_grpc_port,
+        )
+
+        asyncio.get_event_loop().run_until_complete(self.send_to_server(websocket_url))
+        asyncio.get_event_loop().run_forever()
+
+    async def send_to_server(self, websocket_url: str):
+        async with websockets.connect(websocket_url) as websocket:
+            invoice_subscription = self.rpc.subscribe_invoices(settle_index=1)
             for invoice in invoice_subscription:
                 data = {
                     'server_id': get_server_id('invoices'),
@@ -23,15 +40,29 @@ def emit_invoices():
                 log.debug('sending websocket message', data=data)
                 await websocket.send(data_string)
 
-    def worker(loop):
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(send_to_server())
-
-    loop = asyncio.new_event_loop()
-    p = threading.Thread(target=worker, args=[loop])
-    p.start()
-
 
 if __name__ == '__main__':
+    import argparse
 
-    emit_invoices()
+    parser = argparse.ArgumentParser(
+        description='LND Node Operator Tools'
+    )
+
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='store_true',
+        help='Display additional information for debugging'
+    )
+
+    parser.add_argument(
+        '--websocket_url',
+        '-w',
+        type=str,
+        help='The server websocket url',
+        default=DEFAULT_WEBSOCKET_URL
+    )
+
+    args = parser.parse_args()
+
+    InvoiceEmitter(websocket_url=args.websocket_url)
