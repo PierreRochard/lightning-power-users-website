7d46e1599e57e8ddae8e17579c939ff6a1caecd6
Pierre Rochard <pierre@rochard.org>
2019-02-22 13:58:10 -0500

    iterate on home view

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -1,6 +1,80 @@
 
 document.addEventListener('DOMContentLoaded', async function () {
 
+    const capacitySelect = document.querySelector('select[name="capacity"]');
+    const capacityFeeRateSelect = document.querySelector('select[name="capacity_fee_rate"]');
+    const transactionFeeRateSelect = document.querySelector('select[name="transaction_fee_rate"]');
+    capacitySelect.onchange = changeEventHandler;
+    transactionFeeRateSelect.onchange = changeEventHandler;
+    capacityFeeRateSelect.onchange = changeEventHandler;
+    changeEventHandler();
+
+
+    function changeEventHandler() {
+        const capacitySelect = document.querySelector('select[name="capacity"]');
+        const capacityFeeRateSelect = document.querySelector('select[name="capacity_fee_rate"]');
+        const transactionFeeRateSelect = document.querySelector('select[name="transaction_fee_rate"]');
+
+        const selectedCapacity = parseInt(capacitySelect.selectedOptions[0].value);
+        const selectedCapacityUsd = Math.round(selectedCapacity * pricePerSat * 100) / 100;
+        if (selectedCapacity > 0) {
+            capacityFeeRateSelect.disabled = false;
+            console.log(capacityFeeRateSelect.value);
+            if (Math.round(capacityFeeRateSelect.value * 100) === 0) {
+                capacityFeeRateSelect.value = 0.03;
+            }
+            capacityFeeRateSelect.options[0].disabled = true;
+            document.querySelector('#selected-capacity').innerHTML = selectedCapacity.toLocaleString();
+            document.querySelector('#selected-capacity-usd').innerHTML = selectedCapacityUsd.toLocaleString(undefined, {
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2
+            });
+        } else {
+            capacityFeeRateSelect.options[0].disabled = false;
+            capacityFeeRateSelect.value = 0;
+            capacityFeeRateSelect.disabled = true;
+            document.querySelector('#selected-capacity').innerHTML = '-';
+            document.querySelector('#selected-capacity-usd').innerHTML = '-';
+        }
+
+        const selectedCapacityFeeRate = parseFloat(capacityFeeRateSelect.selectedOptions[0].value);
+        document.querySelector('#capacity-fee-rate').innerHTML = selectedCapacityFeeRate.toLocaleString(undefined, {style: 'percent'});
+        document.querySelector('#capacity-fee-rate-usd').innerHTML = selectedCapacityFeeRate.toLocaleString(undefined, {style: 'percent'});
+
+        const capacityFee = Math.round(selectedCapacity * selectedCapacityFeeRate);
+        const capacityFeeUsd = Math.round(capacityFee * pricePerSat * 100) / 100;
+        if (capacityFee > 0) {
+            document.querySelector('#capacity-fee').innerHTML = capacityFee.toLocaleString();
+            document.querySelector('#capacity-fee-usd').innerHTML = capacityFeeUsd.toLocaleString(undefined, {
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2
+            });
+        } else {
+            document.querySelector('#capacity-fee').innerHTML = '-';
+            document.querySelector('#capacity-fee-usd').innerHTML = '-';
+        }
+
+        const selectedTransactionFeeRate = parseInt(transactionFeeRateSelect.selectedOptions[0].value);
+        document.querySelector('#transaction-fee-rate').innerHTML = selectedTransactionFeeRate.toLocaleString();
+
+        const expectedBytes = parseInt(document.querySelector('#expected-bytes').innerHTML);
+        const transactionFee = selectedTransactionFeeRate * expectedBytes;
+        const transactionFeeUsd = Math.round(transactionFee * pricePerSat * 100) / 100;
+        document.querySelector('#transaction-fee').innerHTML = transactionFee.toLocaleString();
+        document.querySelector('#transaction-fee-usd').innerHTML = transactionFeeUsd.toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2
+        });
+
+        const totalFee = transactionFee + capacityFee;
+        const totalFeeUsd = Math.round(totalFee * pricePerSat * 100) / 100;
+        document.querySelector('#total-fee').innerHTML = totalFee.toLocaleString();
+        document.querySelector('#total-fee-usd').innerHTML = totalFeeUsd.toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2
+        });
+    }
+
     const connectForm = document.getElementById('connect_form');
     const connectButton = document.getElementById('connect_button');
     const connectTextarea = document.getElementById('connect_textarea');
@@ -40,7 +114,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 connectError.textContent = msg.error;
                 connectButton.disabled = false;
                 connectTextarea.disabled = false;
-
+                break;
         }
     };
 
--- website/views/home_view.py
+++ website/views/home_view.py
@@ -1,14 +1,32 @@
 import uuid
 
+import structlog
+from bitcoin.core import COIN
 from flask import render_template, session
 from flask_admin import BaseView, expose
 
+from website.utilities.cache.cache import get_latest
+from website.views.request_capacity_view import get_request_capacity_form
 from websocket.constants import DEFAULT_WEBSOCKET_URL
 
 
 class HomeView(BaseView):
     @expose('/')
     def index(self):
+        logger = structlog.get_logger()
+        log = logger.new(request_id=str(uuid.uuid4()))
+        price = get_latest('usd_price')
+        last_price = price['last']
+        price_per_sat = last_price / COIN
+        form = get_request_capacity_form()
+
         if session.get('user_id', None) is None:
             session['user_id'] = uuid.uuid4().hex
-        return render_template('home_view.html', WEBSOCKET_HOST=DEFAULT_WEBSOCKET_URL)
+        if session.get('user_id', None) is None:
+            session['user_id'] = uuid.uuid4().hex
+        return render_template(
+            'home_view/index.html',
+            WEBSOCKET_HOST=DEFAULT_WEBSOCKET_URL,
+            form=form,
+            price_per_sat=price_per_sat
+        )
