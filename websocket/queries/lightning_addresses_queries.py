5f296a7762a28a619ba142ff721afcd73b50c104
Pierre Rochard <pierre@rochard.org>
2019-03-22 19:52:32 -0400

    fix address bug

--- websocket/queries/lightning_addresses_queries.py
+++ websocket/queries/lightning_addresses_queries.py
@@ -1,3 +1,4 @@
+from pprint import pformat
 from typing import List
 
 from lnd_sql import session_scope
@@ -6,11 +7,16 @@ from lnd_sql.models import LightningAddresses
 
 class LightningAddressesQueries(object):
     @staticmethod
-    def get(remote_pubkey: str) -> List[dict]:
+    def get(remote_pubkey: str) -> List[str]:
         with session_scope() as session:
             addresses = (
                 session.query(LightningAddresses).filter(
                     LightningAddresses.pubkey == remote_pubkey)
                     .all()
             )
-            return [a.__dict__ for a in addresses]
+            return [a.address for a in addresses]
+
+
+if __name__ == '__main__':
+    data = LightningAddressesQueries.get('031678745383bd273b4c3dbefc8ffbf4847d85c2f62d3407c0c980430b3257c403')
+    print(pformat(data))
